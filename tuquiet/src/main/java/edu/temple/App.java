89cd61092ae8757116fdeb8888150bfe0244b7bb
RolandG369 <tup55624@temple.edu>
2025-03-18 11:56:50 -0400

    Create and setup app files for TUQuiet use case

--- tuquiet/src/main/java/edu/temple/App.java
+++ tuquiet/src/main/java/edu/temple/App.java
@@ -1,13 +1,23 @@
 package edu.temple;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
- * Hello world!
- *
+ * TUQuiet Application
+ * A web app for finding quiet study spaces at Temple University
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+@RestController
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+    
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to TUQuiet - Find your perfect study space at Temple University!";
     }
 }
