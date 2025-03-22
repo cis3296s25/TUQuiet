ee142a9ed353fe2c67a8ba2c198a14686e4fa6a8
Andrew Coffman <tuq12108@temple.edu>
2025-03-21 21:15:41 -0400

    form functionality minus handling submission. no styling done yet

---
+++ frontend/src/components/ReportingForm.jsx
@@ -0,0 +1,73 @@
+import React, {useState} from "react";
+
+function ReportingForm({ formBuilding }){
+
+    const[noiseLevel, setNoiseLevel] = useState(1);
+    const[crowdLevel, setCrowdLevel] = useState(1);
+    const[description, setDescription] = useState('');
+
+    function updateNoiseLevel(e){
+        setNoiseLevel(e.target.value);
+    }
+
+    function updateCrowdLevel(e){
+        setCrowdLevel(e.target.value);
+    }
+
+    function updateDescription(e){
+        setDescription(e.target.value);
+        //figure out a way to debug this so it doesn't call every letter typed
+    }
+
+    function handleFormSubmission(){
+        //make POST Call here and take user to form page
+    }
+
+    return(
+        <div>
+        <form>
+            <label>Noise Level:</label><br/>
+            <input type="radio" id="level1" name="Noise Level" value="1" onChange={updateNoiseLevel}/>
+            <label>1</label><br/>
+
+            <input type="radio" id="level2" name="Noise Level" value="2" onChange={updateNoiseLevel}/>
+            <label>2</label><br/>
+
+            <input type="radio" id="level3" name="Noise Level" value="3" onChange={updateNoiseLevel}/>
+            <label>3</label><br/>
+
+            <input type="radio" id="level4" name="Noise Level" value="4" onChange={updateNoiseLevel}/>
+            <label>4</label><br/>
+
+            <input type="radio" id="level5" name="Noise Level" value="5" onChange={updateNoiseLevel}/>
+            <label>5</label><br/>
+
+
+            <label>Crowd Level:</label><br/>
+            <input type="radio" id="level1" name="Crowd Level" value="1" onChange={updateCrowdLevel}/>
+            <label>1</label><br/>
+
+            <input type="radio" id="level2" name="Crowd Level" value="2" onChange={updateCrowdLevel}/>
+            <label>2</label><br/>
+
+            <input type="radio" id="level3" name="Crowd Level" value="3" onChange={updateCrowdLevel}/>
+            <label>3</label><br/>
+
+            <input type="radio" id="level4" name="Crowd Level" value="4" onChange={updateCrowdLevel}/>
+            <label>4</label><br/>
+
+            <input type="radio" id="level5" name="Crowd Level" value="5" onChange={updateCrowdLevel}/>
+            <label>5</label><br/>
+
+            <label>Description</label><br/>
+            <input type="text" maxLength="256" onChange={updateDescription}/><br/>
+
+            <button type="submit" onClick={handleFormSubmission}>Submit</button>
+
+        </form>
+        <div>NoiseLevel {noiseLevel}, CrowdLevel {crowdLevel}, Description: {description}</div>
+        </div>
+    );
+}
+
+export default ReportingForm;
