b78a7eb1ac833e5f83bcc39a55be8e1d6e725b54
RolandG369 <tup55624@temple.edu>
2025-03-24 18:23:47 -0400

    Create ReportController.java and implement sample data creation

---
+++ backend/src/main/java/edu/temple/controller/ReportController.java
@@ -0,0 +1,38 @@
+package edu.temple.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+
+@RestController
+@RequestMapping("/api/reports")
+@CrossOrigin(origins = "*")
+public class ReportController {
+    // In mem storage for reports
+    private static final Map<String, List<Map<String, Object>>> locationReports = new HashMap<>();
+
+    // Sample data creation
+    static {
+        // Sample data for 25 locations
+        for (int i = 1; i <= 25; i++) {
+            String locationId = String.valueOf(i);
+            List<Map<String, Object>> reports = new ArrayList<>();
+
+            int reportCount = new Random().nextInt(3) + 1;
+            for (int j = 0; j < reportCount; j++) {
+                reports.add(Map.of(
+                    "noiseLevel", new Random().nextInt(5) + 1,
+                    "crowdLevel", new Random().nextInt(5) + 1
+                ));
+            }
+            locationReports.put(locationId, reports);
+        }
+    }
+
+
+
+
+
+
+}
